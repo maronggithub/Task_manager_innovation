24e2d5649b21bb265fe349d4e0629089fe352df9
MA RONG <marongreceive@gmail.com>
2020-08-06 12:46:59 +0900

    Create the template

---
+++ Main file/js/data.js
@@ -0,0 +1,24 @@
+function saveAllTasks(allTasks) {
+  localStorage.setItem("tasks", JSON.stringify(allTasks));
+}
+
+function getAllTasks() {
+  return JSON.parse(localStorage.getItem("tasks"));
+}
+
+function removeTask(id) {
+  let leftTasks = getAllTasks().filter((task) => task.id !== id);
+  saveAllTasks(leftTasks);
+}
+
+function findTask(id) {
+  return getAllTasks().find((task) => task.id === id);
+}
+
+function findTasksByName(name) {
+  return getAllTasks().filter((task) => task.name.includes(name));
+}
+
+function findTasksByStatus(status) {
+  return getAllTasks().filter((task) => task.status === status);
+}
